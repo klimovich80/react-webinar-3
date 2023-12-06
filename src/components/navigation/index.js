8b17a63e873571f45b15c360c3a03fea908f56fd
System Administrator <root@Air-Admin.IGD_Rostelecom>
2023-12-06 21:04:28 +0300

    dev: pagination styling and display done

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -2,47 +2,41 @@ import React, { memo } from 'react'
 import { cn as bem } from '@bem-react/classname';
 import { INITIAL_PAGE } from '../../constants';
 import './index.css'
+import ReactPaginate from 'react-paginate';
 
 const Navigation = (props) => {
 
-  const setLayout = (page) => {
-    return (
-      <li className={cn('item')} key={page}>
-        <button
-          className={
-            props.recentPage === page
-              ? cn('button active-button')
-              : cn('button')
-          }
-          onClick={props.onSelectPage}
-          value={page}
-        >
-          {page}
-        </button>
-      </li >
-    )
+  const handlePageClick = () => {
+    console.log('handling page click');
   }
 
-  const getContent = (count) => {
-
-    let content = [];
-    content.push(setLayout(INITIAL_PAGE))
-    if (props.pages > 1) {
-      for (let i = INITIAL_PAGE + 1; i < count; i++) {
-        content.push(setLayout(i));
-      }
-      content.push(setLayout(count)
-      )
-    }
-    return content;
-  };
-
   const cn = bem('Navigation')
+
   return (
-    <ul className={cn('items')}>
-      {getContent(props.pages)}
-    </ul>
-  )
+    <>
+      <ReactPaginate
+        nextLabel={null}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={1}
+        pageCount={props.pages}
+        previousLabel={null}
+        pageClassName={cn(`item`)}
+        pageLinkClassName={cn(`link`)}
+        previousClassName={cn(`item`)}
+        previousLinkClassName={cn(`link`)}
+        nextClassName={cn(`item`)}
+        nextLinkClassName={cn(`link`)}
+        breakLabel="..."
+        breakClassName={cn(`item`)}
+        breakLinkClassName={cn(`link`)}
+        containerClassName={cn()}
+        activeClassName="active-item"
+        renderOnZeroPageCount={null}
+        initialPage={INITIAL_PAGE}
+      />
+    </>
+  );
 }
 
 export default memo(Navigation);
