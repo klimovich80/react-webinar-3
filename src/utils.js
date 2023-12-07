df9eeebcf2290f72d661dbc3b685eb8c6105c516
System Administrator <root@Air-Admin.IGD_Rostelecom>
2023-12-08 00:13:47 +0300

    fix: previewyaml

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,7 @@
  */
 export function plural(value, variants = {}, locale = 'ru-RU') {
   // Получаем фурму кодовой строкой: 'zero', 'one', 'two', 'few', 'many', 'other'
+
   // В русском языке 3 формы: 'one', 'few', 'many', и 'other' для дробных
   // В английском 2 формы: 'one', 'other'
   const key = new Intl.PluralRules(locale).select(value);
